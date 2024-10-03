52f920ac36f4d62fac159a5f818a0dc2162d959b
0xMasayoshi <0xMasayoshi@protonmail.com>
2024-10-03 21:56:43 +0800

    chore: finish manta, mode, taiko, & zklink config

--- src/config/features/aggregator.ts
+++ src/config/features/aggregator.ts
@@ -4,6 +4,10 @@ export const AGGREGATOR_ONLY_CHAIN_IDS = [
   ChainId.CRONOS,
   ChainId.MANTLE,
   ChainId.ZKSYNC_ERA,
+  ChainId.MANTA,
+  ChainId.MODE,
+  ChainId.TAIKO,
+  ChainId.ZKLINK,
 ] as const
 
 export type AggregatorOnlyChainId = (typeof AGGREGATOR_ONLY_CHAIN_IDS)[number]
