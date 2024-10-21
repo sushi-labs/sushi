01b94fb2985a1adc59f416a8a1dc440b5fe5f63e
0xMasayoshi <0xMasayoshi@users.noreply.github.com>
2024-10-21 13:42:07 +0000

    chore: lint

--- src/config/wallet.ts
+++ src/config/wallet.ts
@@ -37,7 +37,7 @@ export const MULTISIG_CHAIN_IDS = [
   ChainId.MODE,
   ChainId.TAIKO,
   ChainId.ZKLINK,
-  ChainId.APE
+  ChainId.APE,
 ] as const
 
 export type MultisigChainId = (typeof MULTISIG_CHAIN_IDS)[number]
@@ -79,7 +79,7 @@ export const MULTISIG_ADDRESS: Record<MultisigChainId, `0x${string}`> = {
   [ChainId.MODE]: '0x9ffC84bc9Cf7c85E4E6e2B0aB67BB6c643cA72A2',
   [ChainId.TAIKO]: '0xb8f2faca5b2EBcd6Be7B5D9E164962dE9CCc3B76',
   [ChainId.ZKLINK]: '0x0b0b61eC14f6b7AEfE62C460103E1a4638546298',
-  [ChainId.APE]: '0xe7BEF4bFb8AcBe2b68B262A2323e09f13c021a9b'
+  [ChainId.APE]: '0xe7BEF4bFb8AcBe2b68B262A2323e09f13c021a9b',
 } as const
 
 export const isMultisigChainId = (
