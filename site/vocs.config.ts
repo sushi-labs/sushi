e8835332e88690a8ca29dbe305eb443ed564c50d
Matthew Lilley <hello@matthewlilley.com>
2025-01-09 04:24:29 +0000

    chore(docs): add favicons

--- site/vocs.config.ts
+++ site/vocs.config.ts
@@ -16,6 +16,8 @@ export default defineConfig({
     pattern: 'https://github.com/sushi-labs/sushi/edit/master/site/pages/:path',
     text: 'Suggest changes to this page',
   },
+  iconUrl: { light: '/favicons/light.png', dark: '/favicons/dark.png' },
+  // logoUrl: { light: '/icon-light.png', dark: '/icon-dark.png' },
   sidebar,
   rootDir: '.',
   topNav: [
