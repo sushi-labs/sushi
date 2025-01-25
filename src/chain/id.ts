22b65020599c71054a9ee38bd59b3e5a110427c2
LufyCZ <me@lufy.cz>
2025-01-25 13:44:50 +0000

    feat: add isChainId

--- src/chain/id.ts
+++ src/chain/id.ts
@@ -9,3 +9,6 @@ export const ChainId = {
 } as const
 
 export type ChainId = (typeof ChainId)[keyof typeof ChainId]
+
+export const isChainId = (chainId: number): chainId is ChainId =>
+  Object.values(ChainId).includes(chainId as ChainId)
