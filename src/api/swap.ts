863b17e1fb4cd4e79b52ff4285f8851188915e9f
LufyCZ <me@lufy.cz>
2025-02-08 15:42:54 +0000

    feat(sushi/api): improve getSwap function

--- src/api/swap.ts
+++ src/api/swap.ts
@@ -1,58 +1,151 @@
 import type { Address } from 'viem'
-import type { Hex } from 'viem'
-import type { RouteStatus } from '../router/route-status.js'
-import type { TransferValue } from '../router/transfer-value.js'
+import { z } from 'zod'
+import type { ExtractorSupportedChainId } from '../config/index.js'
+import { version } from '../package.json'
+import { RouteStatus } from '../router/route-status.js'
+import { TransferValue } from '../router/transfer-value.js'
+import { sz } from '../validate/zod.js'
 
-export interface SwapRequest {
-  chainId: number
+type To<Required extends boolean> = Required extends true
+  ? { to: Address }
+  : { to?: Address }
+
+type Fee<Enabled extends boolean> = Enabled extends true
+  ? {
+      enableFee: Enabled
+      fee: bigint
+      feeReceiver: Address
+      feeBy?: TransferValue
+    }
+  : {
+      enableFee?: Enabled
+      fee?: bigint
+      feeReceiver?: Address
+      feeBy?: TransferValue
+    }
+
+export type SwapRequest<
+  IncludeTransaction extends boolean,
+  EnableFee extends boolean,
+> = {
+  chainId: ExtractorSupportedChainId
   tokenIn: Address
   tokenOut: Address
   amount: bigint
   maxSlippage: number
   maxPriceImpact?: number
-  gasPrice?: bigint
-  to?: Address
-  enableFee?: boolean
-  feeReceiver?: Address
-  fee?: bigint
-  feeBy?: TransferValue
-  includeTransaction?: boolean
-}
+  includeTransaction?: IncludeTransaction
+  fee?: Fee<EnableFee>
+  referrer?: string
+} & To<IncludeTransaction>
 
-export type SwapResponse =
-  | { status: typeof RouteStatus.NoWay }
-  | {
-      status: typeof RouteStatus.Success | typeof RouteStatus.Partial
-      swapPrice: number
-      priceImpact: number
-      amountIn: string
-      assumedAmountOut: string
-      tx: { from: Address; to: Address; data: Hex; value: bigint }
+function swapResponseSchema<IncludeTransaction extends boolean>(
+  includeTransaction?: IncludeTransaction,
+) {
+  const baseSuccessPartial = z.object({
+    status: z.literal(RouteStatus.Success).or(z.literal(RouteStatus.Partial)),
+    tokens: z.array(
+      z.object({
+        address: sz.address(),
+        decimals: z.number(),
+        symbol: z.string(),
+        name: z.string(),
+      }),
+    ),
+    tokenFrom: z.number(),
+    tokenTo: z.number(),
+
+    swapPrice: z.number(),
+    priceImpact: z.number(),
+
+    amountIn: z.string(),
+    assumedAmountOut: z.string(),
+  })
+
+  const baseNoWay = z.object({
+    status: z.literal(RouteStatus.NoWay),
+  })
+
+  const txSchema = z.object({
+    from: sz.address(),
+    to: sz.address(),
+    data: sz.hex(),
+    value: z.string(),
+  })
+
+  const baseSchema = baseSuccessPartial.or(baseNoWay)
+  const baseTxSchema = baseSuccessPartial.extend({ tx: txSchema }).or(baseNoWay)
+
+  type Result = IncludeTransaction extends true
+    ? typeof baseTxSchema
+    : typeof baseSchema
+
+  const schema = includeTransaction ? baseTxSchema : (baseSchema as Result)
+
+  return schema.transform((data) => {
+    if (data.status === RouteStatus.NoWay) {
+      return data
     }
 
-export async function getSwap(params: SwapRequest, options?: RequestInit) {
+    return {
+      ...data,
+      tokenFrom: data.tokens[data.tokenFrom],
+      tokenTo: data.tokens[data.tokenTo],
+    }
+  })
+}
+
+export type SwapResponse<IncludeTransaction extends boolean> = z.infer<
+  ReturnType<typeof swapResponseSchema<IncludeTransaction>>
+>
+
+export async function getSwap<
+  IncludeTransaction extends boolean = false,
+  EnableFee extends boolean = false,
+>(
+  params: SwapRequest<IncludeTransaction, EnableFee>,
+  options?: RequestInit,
+): Promise<SwapResponse<IncludeTransaction>> {
   // TODO: VALIDATE PARAMS
   const url = new URL(`swap/v5/${params.chainId}`, 'https://api.sushi.com')
+
   url.searchParams.append('tokenIn', params.tokenIn)
   url.searchParams.append('tokenOut', params.tokenOut)
   url.searchParams.append('amount', params.amount.toString())
   url.searchParams.append('maxSlippage', params.maxSlippage.toString())
-  if (params?.maxPriceImpact)
+
+  if (params.maxPriceImpact) {
     url.searchParams.append('maxPriceImpact', params.maxPriceImpact.toString())
-  if (params?.gasPrice)
-    url.searchParams.append('gasPrice', params.gasPrice.toString())
-  if (params?.to) url.searchParams.append('to', params.to)
-  if (params?.enableFee && params?.fee && params?.feeReceiver) {
-    url.searchParams.append('enableFee', params.enableFee.toString())
-    url.searchParams.append('fee', params.fee.toString())
-    url.searchParams.append('feeReceiver', params.feeReceiver)
-    if (params?.feeBy) url.searchParams.append('feeBy', params.feeBy)
   }
-  if (params?.includeTransaction)
+
+  if (params.to) {
+    url.searchParams.append('to', params.to)
+  }
+
+  if (params.fee?.enableFee) {
+    url.searchParams.append('enableFee', params.fee.enableFee.toString())
+    url.searchParams.append('fee', params.fee.fee.toString())
+    url.searchParams.append('feeReceiver', params.fee.feeReceiver)
+
+    if (params.fee.feeBy) {
+      url.searchParams.append('feeBy', params.fee.feeBy)
+    }
+  }
+
+  if (params.includeTransaction) {
     url.searchParams.append(
       'includeTransaction',
       params.includeTransaction.toString(),
     )
+  }
+
+  url.searchParams.append('referrer', params.referrer ?? `sushi-sdk/${version}`)
+
   const res = await fetch(url.toString(), options)
-  return res.json() as Promise<SwapResponse>
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch swap: ${await res.text()}`)
+  }
+
+  return swapResponseSchema(params.includeTransaction).parse(await res.json())
 }
