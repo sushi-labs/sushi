bbaa2c68f97858e9b1864b86b446bed1efe1fecd
Daniel Reinoso <daniel@sushi.com>
2025-03-05 21:27:31 -0300

    feat: add blade chains

---
+++ src/config/features/blade.ts
@@ -0,0 +1,19 @@
+import { EvmChainId } from '../../chain/evm/index.js'
+
+export const BLADE_SUPPORTED_CHAIN_IDS = [
+  EvmChainId.ETHEREUM,
+  EvmChainId.POLYGON,
+  EvmChainId.POLYGON_ZKEVM,
+  EvmChainId.OPTIMISM,
+  EvmChainId.ARBITRUM,
+  EvmChainId.MOONBEAM,
+  EvmChainId.BASE,
+  EvmChainId.MANTLE,
+] as const
+
+export type BladeChainId = (typeof BLADE_SUPPORTED_CHAIN_IDS)[number]
+
+export const isBladeChainId = (
+  chainId: EvmChainId,
+): chainId is BladeChainId =>
+  BLADE_SUPPORTED_CHAIN_IDS.includes(chainId as BladeChainId)
