75daf2d9e0616acae1bbc8e6223b8bd6d73819dc
OlaStenberg <hello@olastenberg.com>
2025-03-12 10:57:31 +0100

    feat(src/api): use v6

--- src/api/swap.test.ts
+++ src/api/swap.test.ts
@@ -8,32 +8,39 @@ const baseSwapRequest = {
   amount: 1000000000000000000n,
   maxSlippage: 0.005,
   gasPrice: 8563095715n,
+  sender: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'
 } as const
 
-const to = '0x8f54C8c2df62c94772ac14CcFc85603742976312' as const
 
 describe('getSwap', () => {
-  it('should return a swap when to is omitted', async () => {
-    const result = await getSwap(baseSwapRequest)
 
-    expect(result).include({ status: 'Success' })
-  })
-  it('should return a swap when to is included', async () => {
+  it('should return a swap when recipient is included', async () => {
     const result = await getSwap({
       ...baseSwapRequest,
-      to,
+      recipient: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'
     })
 
     expect(result).include({ status: 'Success' })
   })
-  it('should return a swap tx when includeTransaction is true', async () => {
+
+  it('should include a swap with tx.gas when simulate is true', async () => {
     const result = await getSwap({
       ...baseSwapRequest,
-      to,
-      includeTransaction: true,
+      simulate: true,
+    })
+    
+    expect(result).include({ status: 'Success' })
+    if (result.status === 'Success') {
+      expect(result.tx).include.keys('gas')
+    } 
+  })
+
+  it.skip('should include a swap when url is set to staging true', async () => {
+    const result = await getSwap({
+      ...baseSwapRequest,
+      baseUrl: 'https://staging.sushi.com',
     })
 
     expect(result).include({ status: 'Success' })
-    expect(result).include.keys('tx')
   })
 })
