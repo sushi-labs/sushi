613665c1afc0e33b5530e31eb6a2b300c4e6d756
OlaStenberg <29061789+OlaStenberg@users.noreply.github.com>
2025-03-27 11:25:32 +0000

    chore: lint

--- src/config/features/sushiswap-v2.ts
+++ src/config/features/sushiswap-v2.ts
@@ -104,7 +104,7 @@ export const SUSHISWAP_V2_INIT_CODE_HASH: Record<
   // [EvmChainId.HECO_TESTNET]:
   //   '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
   // [EvmChainId.HARMONY]:
-    // '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
+  // '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
   // [EvmChainId.HARMONY_TESTNET]:
   //   '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
   // [EvmChainId.OKEX]:
