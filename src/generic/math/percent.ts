bd7c778a1be82f1e2d4327cd2aa01dd0df9f5baa
LufyCZ <me@lufy.cz>
2025-07-04 18:07:14 +0000

    fix: percent toString

--- src/generic/math/percent.ts
+++ src/generic/math/percent.ts
@@ -4,9 +4,9 @@ import { Fraction } from './fraction.js'
 export class Percent extends Fraction {
   /**
    *
-   * @param args.maxFixed - The maximum number of fixed decimal places to display, only if relevant, eg. "0%" for 0, "1.23%" for 0.0123, etc.
-   * @param args.fixed - The number of fixed decimal places to display, always, eg. "0.00%" for 0, "1.23%" for 0.0123, etc.
-   * @returns A string representation of the percent value, eg. "1.53%"
+   * @param args.maxFixed - The maximum number of fixed decimal places to display, only if relevant, eg. "0" for 0, "1.23" for 0.0123, etc.
+   * @param args.fixed - The number of fixed decimal places to display, always, eg. "0.00" for 0, "1.23" for 0.0123, etc.
+   * @returns A string representation of the percent value, eg. "1.53"
    */
   public override toString(
     args: Parameters<typeof numberToFixed>[1] = { fixed: 2 },
@@ -14,6 +14,12 @@ export class Percent extends Fraction {
     const num = this.toNumber() * 100
     const str = numberToFixed(num, args)
 
-    return `${str}%`
+    return str
+  }
+
+  public toPercentString(
+    args: Parameters<typeof numberToFixed>[1] = { fixed: 2 },
+  ): string {
+    return `${this.toString(args)}%`
   }
 }
