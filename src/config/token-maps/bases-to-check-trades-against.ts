77eae8955872871af4373d3df2fb68fc9d9366d5
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-07-09 01:57:58 +0700

    fix: hyperevm bases

--- src/config/token-maps/bases-to-check-trades-against.ts
+++ src/config/token-maps/bases-to-check-trades-against.ts
@@ -648,5 +648,5 @@ export const BASES_TO_CHECK_TRADES_AGAINST: {
       decimals: 18,
     }),
   ],
-  [EvmChainId.HYPEREVM]: [WNATIVE[EvmChainId.HYPEREVM]],
+  [EvmChainId.HYPEREVM]: [WNATIVE[EvmChainId.HYPEREVM], USDT[EvmChainId.HYPEREVM]],
 }
