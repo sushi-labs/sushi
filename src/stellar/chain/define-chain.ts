eefee59d5a3e43519d88c4af7671e871e7ea57ba
xBalbinus <xiangan9@gmail.com>
2025-12-11 15:15:23 -0500

    add stellar as a supported chain

---
+++ src/stellar/chain/define-chain.ts
@@ -0,0 +1,46 @@
+import type {
+  BlockExplorers,
+  Chain,
+  NetType,
+} from '../../generic/chain/interface.js'
+import type { StellarChainId, StellarChainKey } from './chains.js'
+
+export type StellarChainType = 'stellar'
+
+type StellarChainBase<TChainId extends number, TChainKey extends string> =
+  Chain<
+    StellarChainType,
+    // @ts-expect-error prevent infinite loop
+    TChainId,
+    TChainKey,
+    Readonly<string>,
+    Readonly<string>,
+    NetType,
+    BlockExplorers,
+    string,
+    string
+  >
+
+export type StellarChain = StellarChainBase<StellarChainId, StellarChainKey>
+
+type StellarChainInput = Omit<
+  StellarChainBase<number, string>,
+  'type' | 'getTransactionUrl' | 'getAccountUrl' | 'getTokenUrl'
+>
+
+export function defineStellarChain<const T extends StellarChainInput>(
+  chain: T,
+) {
+  return {
+    ...chain,
+    type: 'stellar' as const,
+
+    getTransactionUrl: (input: string) =>
+      `${chain.blockExplorers.default.url}/tx/${input}`,
+    getAccountUrl: (input: string) =>
+      `${chain.blockExplorers.default.url}/account/${input}`,
+    getTokenUrl: (input: string) =>
+      `${chain.blockExplorers.default.url}/asset/${input}`,
+  } as const satisfies StellarChainBase<number, string>
+}
+
---
+++ src/stellar/chain/index.ts
@@ -0,0 +1,3 @@
+export * from './chains.js'
+export * from './define-chain.js'
+
