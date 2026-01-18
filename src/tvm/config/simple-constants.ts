82ec188dae173f7d278335988a538d8a2f680d76
LufyCZ <20911347+LufyCZ@users.noreply.github.com>
2026-01-18 16:57:02 +0000

    chore: lint

--- src/tvm/config/simple-constants.ts
+++ src/tvm/config/simple-constants.ts
@@ -1,4 +1,3 @@
 import type { TvmAddress } from '../currency/token.js'
 
-export const tvmNativeAddress: TvmAddress =
-  'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb'
+export const tvmNativeAddress: TvmAddress = 'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb'
