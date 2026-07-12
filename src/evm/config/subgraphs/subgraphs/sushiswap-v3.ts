d208e4475352fb6fa2bde4ed5d94ca6c6b05b4cc
LufyCZ <me@lufy.cz>
2026-07-12 15:15:33 +0000

    feat: add robinhood subgraphs

--- src/evm/config/subgraphs/subgraphs/sushiswap-v3.ts
+++ src/evm/config/subgraphs/subgraphs/sushiswap-v3.ts
@@ -27,6 +27,7 @@ const SUSHISWAP_V3_OTHER_URLS = {
   [EvmChainId.ROOTSTOCK]: `${SUSHI_DEDICATED_GOLDSKY_HOST}/sushiswap/v3-rootstock-3/gn`,
   [EvmChainId.HEMI]: `${SUSHI_DEDICATED_GOLDSKY_HOST}/sushiswap/v3-hemi/gn`,
   [EvmChainId.KATANA]: `${SUSHI_DEDICATED_GOLDSKY_HOST}/sushiswap/v3-katana/gn`,
+  [EvmChainId.ROBINHOOD]: `${SUSHI_DEDICATED_GOLDSKY_HOST}/sushiswap/v3-robinhood/gn`,
 } as const satisfies Partial<Record<SushiSwapV3ChainId, string>>
 
 export const getSushiSwapV3SubgraphUrl = getSubgraphUrlWrapper({
