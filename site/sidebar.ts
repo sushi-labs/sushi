2baf4184fc8954be9f23ed7928b929a7781a901a
LufyCZ <me@lufy.cz>
2026-08-06 20:24:18 +0000

    fix: reorder amm links

--- site/sidebar.ts
+++ site/sidebar.ts
@@ -235,9 +235,9 @@ export const sidebar = {
           text: 'AMM',
           collapsed: true,
           items: [
+            { text: 'SushiSwap V2', link: '/contracts/cpamm' },
             { text: 'SushiSwap V3', link: '/contracts/clamm' },
             { text: 'SushiSwap V4', link: '/contracts/v4' },
-            { text: 'SushiSwap V2', link: '/contracts/cpamm' },
           ],
         },
         {
