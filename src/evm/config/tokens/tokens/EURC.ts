c37a57acaa66b824d64c58ebc438fad7ffe63629
LufyCZ <me@lufy.cz>
2026-09-16 13:28:57 +0000

    fix(evm): update Arc EURC address

--- src/evm/config/tokens/tokens/EURC.ts
+++ src/evm/config/tokens/tokens/EURC.ts
@@ -2,7 +2,7 @@ import { EvmChainId } from '../../../chain/index.js'
 import { addressMapToTokenMap } from '../address-map-to-token-map.js'
 
 export const EURC_ADDRESS = {
-  [EvmChainId.ARC]: '0x89b50855aa3be2f677cd6303cec089b5f319d72a',
+  [EvmChainId.ARC]: '0xbef5f6d51cb62b58e6a8f77868681825c6fe21c1',
 } as const
 
 export const EURC = addressMapToTokenMap(
